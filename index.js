1c70595788260c495a742b0c84485a20d9d0e1ac
tomica09 <86150037+tomica09@users.noreply.github.com>
2021-07-21 10:20:47 +0900

    Update and rename 진짜로열심히 할 js봇 만들기 시험 to index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
 client.on('ready', () => {
 	console.log(`${client.user.tag}을 로그인 했습니다!`);
-});
+})
 
 client.on('message', msg => {
     if(msg.content === "주사위ㄱ"){
       const random = Math.floor(Math.random() * 6 + 1);
       const embed = new Discord.MessageEmbed()
-        .setColor('#00fdc3')
-        .setTitle(`${random}`)
-        .addField('↑주사위 결과', 'MN봇 만들기, 이번엔 열심히 할게요....')
-      msg.reply(embed)
+        	.setColor('#00fdc3')
+        	.setTitle(`${random}`)
+        	.addField('↑주사위 결과', 'MN봇 만들기, 이번엔 열심히 할게요....')
+      msg.channel.send(embed)
     }
 });
 
